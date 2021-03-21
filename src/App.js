3e65d23d0da7b086c440fd5e74f61637796538b7
Nisha <nisha00396@gmail.com>
2021-03-22 03:53:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [text, setText] = useState(null);
+  const [city, setCity] = useState(null);
+
   return (
-    <div>Today's Weather
+    <div className="box">
+      <div className="inputData">
+        <input className="inputField"
+          type="text"
+          onChange={(event) => {
+
+          }} />
+      </div>
+
+      <div className="info">
+        <h2 className="location">
+          <i class="fas fa-street-view">{text}</i>
+        </h2>
+        <p id="date"> </p>
+        <h1 className="temp"></h1>
+        <h3 className="tempmin_max"> </h3>
+      </div>
+
+      <div className="wave -one"></div>
+      <div className="wave -two"></div>
+      <div className="wave -three"></div>
+
+      <div id="weathercon">
+        <i class="fa"></i>
+      </div>
+
     </div>
+
   );
 }
 
