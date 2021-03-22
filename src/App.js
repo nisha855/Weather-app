f6930c8f28c9d8446adc5486f64ae486d4b6b825
Nisha <nisha00396@gmail.com>
2021-03-22 15:26:56 +0530

    time added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  var tempDate = new Date();
+  var date = "Date:" + tempDate.getFullYear() + '-' + (tempDate.getMonth() + 1) + '-' + tempDate.getDate() + '   ' + "Time:" + tempDate.getHours() + ':' + tempDate.getMinutes() + ':' + tempDate.getSeconds();
+  const currDate = date;
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState("Mumbai");
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=82de51691bb5e32cbfab69ec0f312f1b`;
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=82de51691bb5e32cbfab69ec0f312f1b`;
       const response = await fetch(url);
       const users = await response.json();
-      setCity(users.main);
+      setCity(users);
     }
     fetchData();
   }, [search])
-
+  const store = city.weather[0].main;
   return (
     <>
+
       <div className="box">
         <div className="inputData">
           <input className="inputField"
@@ -34,11 +38,12 @@ const App = () => {
               <h2 className="location">
                 <i className="fas fa-street-view"></i>{search}
               </h2>
-              <p id="date"> </p>
+
+              <p id="date">{currDate}</p>
               <h1 className="temp">
-                {city.temp}°C
+                {city.main.temp}°C
             </h1>
-              <h3 className="tempmin_max">Min:{city.temp_min}°C | Max:{city.temp_max}°C </h3>
+              <h3 className="tempmin_max">Min:{city.main.temp_min}°C | Max:{city.main.temp_max}°C </h3>
             </div>
 
             <div className="wave -one"></div>
